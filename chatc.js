f92dcbc75fa9481537a46118ac90f4cc14bbc68d
yinizhizhu <lijiahe19941013@sina.cn>
2017-04-20 09:15:51 +0800

    update

--- chatc.js
+++ chatc.js
@@ -1,4 +1,5 @@
-var builder = require("botbuilder");
+var builder = require('botbuilder');
+var restify = require("restify");
 
 // create the connector
 var connector = new builder.ChatConnector();
@@ -8,9 +9,15 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     function(session) {
-        builder.Prompts.text(session, "What is your name?");
+        builder.Prompts.text(session, "Please enter your name");
     },
     function(session, result) {
         session.send('Hello, ' + result.response);
     }
 ]);
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
